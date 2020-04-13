0ebd85897767d65607ee9f00a358cdb8e6aaab74
garret.valentino@gmail.com <garret.valentino@gmail.com>
2020-04-13 15:44:48 -0500

    this is the humane society project first commit with the data base added in the starter code

--- HumaneSociety/Properties/Settings.Designer.cs
+++ HumaneSociety/Properties/Settings.Designer.cs
@@ -22,5 +22,16 @@ namespace HumaneSociety.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=GARRETSLAPTOP\\SQLEXPRESS02;Initial Catalog=HumaneSociety2;Integrated " +
+            "Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False")]
+        public string HumaneSociety2ConnectionString {
+            get {
+                return ((string)(this["HumaneSociety2ConnectionString"]));
+            }
+        }
     }
 }
